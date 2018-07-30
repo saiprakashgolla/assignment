76ac002f0cc067d71f638b75382a0b046bb5f3b5
saiprakashgolla <saiprakash.g@mtwlabs.com>
2018-07-30 13:44:32 +0530

    task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,39 +7,14 @@ import { TabsModule } from 'ngx-bootstrap';
 import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTabsModule } from '@angular/material/tabs';
+
 
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
-import { CarsComponent } from './cars/cars.component';
-import { ElectronicsComponent } from './electronics/electronics.component';
-import { MobileComponent } from './mobile/mobile.component';
-import { FurnitureComponent } from './furniture/furniture.component';
-import { BikesComponent } from './bikes/bikes.component';
-import { BooksComponent } from './books/books.component';
-import { FashionComponent } from './fashion/fashion.component';
-import { PetsComponent } from './pets/pets.component';
-import { ServicesComponent } from './services/services.component';
-import { JobsComponent } from './jobs/jobs.component';
-import { PropertiesComponent } from './properties/properties.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { SubmitAdComponent } from './submit-ad/submit-ad.component';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { LoginService } from './app.service';
-import { CarSaleComponent } from './car-sale/car-sale.component';
-import { loginServices } from './login.services';
-import { HttpClientModule } from '@angular/common/http';
-import{ FilterPipe} from './app.filter';
-import { MyaccountComponent } from './myaccount/myaccount.component';
-import { SubmitAddService } from './submitAdd.service';
-import { RegisterService } from './register.service';
 import { CategorynamesComponent } from './categorynames/categorynames.component';
-import { StudentslistComponent } from './studentslist/studentslist.component';
-import { StudentdetailsComponent } from './studentdetails/studentdetails.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 
 
@@ -47,12 +22,7 @@ import { StudentdetailsComponent } from './studentdetails/studentdetails.compone
 
 const routes: Routes = ([
 
-  { path: '', component: HomeComponent },
-  { path: "signup", component: SignupComponent },
-  { path: "login", component: LoginComponent },
-  { path: "submit-ad", component: SubmitAdComponent },
-  { path: 'carSale', component: CarSaleComponent },
-  { path: 'myaccount', component: MyaccountComponent},
+
 
   {
     path: 'categories', component: CategoriesComponent,
@@ -61,9 +31,7 @@ const routes: Routes = ([
      { path : ':id', component : CategorynamesComponent }
      ]
   },
-  {path: 'student-list', component : StudentslistComponent},
 
-  { path : 'student-list/:num:' , component : StudentdetailsComponent }
     
   
 
@@ -73,44 +41,22 @@ const routes: Routes = ([
   declarations: [
     AppComponent,
     CategoriesComponent,
-    CarsComponent,
-    ElectronicsComponent,
-    MobileComponent,
-    FurnitureComponent,
-    BikesComponent,
-    BooksComponent,
-    FashionComponent,
-    PetsComponent,
-    ServicesComponent,
-    JobsComponent,
-    PropertiesComponent,
     NavbarComponent,
-    HomeComponent,
     FooterComponent,
-    SignupComponent,
-    LoginComponent,
-    SubmitAdComponent,
-    CarSaleComponent,
-    FilterPipe,
-    MyaccountComponent,
     CategorynamesComponent,
-    StudentslistComponent,
-    StudentdetailsComponent
-
-  ],
+   ],
   imports: [
     BrowserModule,
     FormsModule,
     TabsModule.forRoot(),
     RouterModule.forRoot(routes),
-    ModalModule.forRoot(),
     AccordionModule.forRoot(),
     BsDropdownModule.forRoot(),
     BrowserAnimationsModule,
-    MatTabsModule,
-    HttpClientModule
+   
+   
   ],
-  providers: [RegisterService,loginServices,SubmitAddService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
